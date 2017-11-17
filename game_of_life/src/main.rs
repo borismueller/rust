8c92a64004d7c99184ff21cce13be9256bbcd8ff
borismueller <boris.mueller@iet-gibb.ch>
2017-11-17 20:26:39 +0100

    working now

--- game_of_life/src/main.rs
+++ game_of_life/src/main.rs
@@ -4,13 +4,13 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
-    println!("Hello, world!");
     //world is our field, it holds booleans which say wether the cell is alive or not
-    let mut world = init_world(3);
-    let episodes = 2;
+    let mut world = init_world(5);
+    let episodes = 10;
     for i in 0..episodes {
         println!("Episode: {0}", i);
         print_world(&world);
+        get_num_neighbors(&world, 1, 0);
         world = apply_rules(world);
     }
 }
@@ -35,7 +35,7 @@ fn print_world(world: &Vec<Vec<bool>>) {
             if cell {
                 print!("* ");
             } else {
-                print!("_ ");
+                print!("- ");
             }
         }
         print!("\n");
@@ -46,12 +46,12 @@ fn random_bool() -> bool {
     //let's make our own, bad, randomness
     //get current time in ms
     let seed = SystemTime::now().duration_since(UNIX_EPOCH)
-        .expect("Time went backwards"); 
-    let s: usize = seed.subsec_nanos() as usize; 
-    
+        .expect("Time went backwards");
+    let s: usize = seed.subsec_nanos() as usize;
+
     //get our random number with our seed
     let rand = lcg(134456, 8121, 28411, s);
-    
+
     //return our random bool based on our number
     return rand % 2 == 0
 }
@@ -62,47 +62,47 @@ fn lcg(m: usize, a: usize, c: usize, s: usize) -> usize {
     (a * s + c) % m
 }
 
-fn apply_rules(mut world: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+fn apply_rules(world: Vec<Vec<bool>>) -> Vec<Vec<bool>> {
     //Apply the rules to our world and return the resulting world
+    let mut new_world = world.clone(); //new world because the rules don't work out otherwise
     for i in 0..world.len() {
         for j in 0..world[i].len() {
-            //doesn't work yet
             let num_alive = get_num_neighbors(&world, i, j);
             if world[i][j] {
-                println!("cell {0} {1} is alive", i, j);
-                //cell is alive   
+                //cell is alive
                 if num_alive < 2 {
-                    world[i][j] = false;
+                    new_world[i][j] = false;
                 } else if num_alive > 3 {
-                    world[i][j] = false;
+                    new_world[i][j] = false;
                 }
             } else {
                 //cell is dead
                 if num_alive == 3 {
-                    world[i][j] = true;
+                    new_world[i][j] = true;
                 }
             }
         }
     }
-    world
+    new_world
 }
 
 fn get_num_neighbors(world: &Vec<Vec<bool>>, i: usize, j: usize) -> usize {
-    //return the number of neighbors for the cell at index in world 
+    //return the number of neighbors for the cell at index in world
     let mut num = 0;
     let i = i as i32;
     let j = j as i32;
-    for k in -2..4 {
-        //go through all neighbors
-        if !(i+k < 0 || i+k > (world.len() as i32 -1)) {
-            if !(j+k < 0 || j+k > (world[0].len() as i32 -1)) {
-                //make sure we aren't out of bounds
-                if world[(i+k) as usize][(j+k) as usize] {
-                    num += 1;
+
+    for k in -1..2 {
+        if !(i+k < 0 || i+k >= world.len() as i32) {
+            for l in -1..2{
+                if !(j+l < 0 || j+l >= world[0].len() as i32 || (k == 0 && l == 0)) {
+                    //make sure we aren't out of bounds, or on the same field
+                    if world[(i+k) as usize][(j+l) as usize] {
+                        num += 1;
+                    }
                 }
             }
         }
     }
     num
 }
-
